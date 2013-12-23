6d008e764c8892c179fbd944dff152ae3df2edcd
oriroi <orisan3@gmail.com>
2013-12-23 07:45:39 +0000

    add calc, spawn

---
+++ c/calc.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int
+main(int argc, char *argv[])
+{
+
+//    if (argc != 2) {
+//        fprintf(stderr, "Usage: %s n\n", argv[0]);
+//        exit(1);
+//    }
+    //long sin_value = atol(argv[1]);
+    //long sin_value = atol(1);
+    //if (sin_value != NULL) {
+        printf("sin_value %d\n", sin(15));
+    //}
+    exit(0);
+}
+
