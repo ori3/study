6bdf19021a4412d7a12545e7fc1942b16ac25c46
oriroi <vagrant@vagrant-debian-squeeze.vagrantup.com>
2014-12-28 13:19:26 +0000

    rails4 study commit

---
+++ rails4/taskapp/app/controllers/hoges_controller.rb
@@ -0,0 +1,2 @@
+class HogesController < ApplicationController
+end
---
+++ rails4/taskapp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taskapp::Application.config.secret_key_base = 'b5c51fe9c85e7d6b98138061b1a49ce22b09383da91236d31b4b91741ee70f21039b17f3d494acd85dbcf58657be81b0d4ca56b837ce40febdef7beb4fcf184e'
