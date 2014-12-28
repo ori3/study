16c7a8c3175f82438330e8b6878635e387ea0dde
oriroi <vagrant@vagrant-debian-squeeze.vagrantup.com>
2014-12-28 13:53:25 +0000

    append code of rails4

--- rails4/taskapp/app/controllers/projects_controller.rb
+++ rails4/taskapp/app/controllers/projects_controller.rb
@@ -1,11 +1,12 @@
 class ProjectsController < ApplicationController
 
+  before_action :set_project, only: [:show, :edit, :update, :destroy]
+
   def index
     @projects = Project.all
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def new
@@ -21,10 +22,31 @@ class ProjectsController < ApplicationController
     end
   end
 
+
+  def edit
+  end
+
+  def update
+    if @project.update(project_params)
+        redirect_to projects_path
+    else
+        render 'edit'
+    end
+  end
+
+  def destroy
+    @project.destroy
+    redirect_to projects_path
+  end
+
   private
 
     def project_params
       params[:project].permit(:title)
+    end
+
+    def set_project
+      @project = Project.find(params[:id])
     end
 
 end
